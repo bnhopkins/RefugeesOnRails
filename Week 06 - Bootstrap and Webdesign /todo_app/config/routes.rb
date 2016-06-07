2d6373a12447fd7a047a5a26848bd00bbb86f4fa
Brooke Hopkins <brooke.hopkins@nyu.edu>
2016-06-07 14:58:37 +0200

    blog

--- Week 06 - Bootstrap and Webdesign /todo_app/config/routes.rb
+++ Week 06 - Bootstrap and Webdesign /todo_app/config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
-  resources :articles do
-    resources :comments
-  end
+  resources :todos
+  root 'todos#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- Week 06 - Bootstrap and Webdesign /todo_app/db/schema.rb
+++ Week 06 - Bootstrap and Webdesign /todo_app/db/schema.rb
@@ -11,23 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606070112) do
+ActiveRecord::Schema.define(version: 20160531160043) do
 
-  create_table "articles", force: :cascade do |t|
+  create_table "todos", force: :cascade do |t|
     t.string   "title"
-    t.text     "text"
+    t.text     "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
